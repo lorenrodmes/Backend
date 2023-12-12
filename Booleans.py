d681915049140f3a638761b49f99410089172c65
lorenrodmes <rlorename924@gmail.com>
2023-12-12 17:32:18 -0500

    En este avance se finalizo el segundo bloque de temática del curso de Python, el cual se finalizo con la tematica de booleanos, transpormacion de datos y templates

---
+++ Booleans.py
@@ -0,0 +1,38 @@
+name = "Lore"
+print(type(name))
+name = 27
+print(type(name))
+name = True
+print(type(name))
+
+print("Lore" + " Rodriguez ")
+
+print(10 + 20)
+print("Lore" + "27")
+
+age = 27
+print("Mi edad es " + str(age))
+print(f"Mi edad es {age}")
+
+age = input('Escribe tu edad => ')
+print(type(age))
+age = int(age)
+age += 10
+print(f'Tu edad en 10 años será {age}')
+
+nombre = "Lorenita"
+edad = 25
+print(f"Hola mi nombre es {nombre} y tengo {edad} años de edad")
+
+# Ejercicio finalizacion de bloque en platzi 
+
+name = 'Juana'
+print(name)
+age = '10'
+print(age)
+total = int(age) + 10
+
+print (f" Hola mi nombre es {name}, tengo {age} años y en 10 años tendré {total} años")
+
+
+
