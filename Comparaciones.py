e89a7d4e51cc0155abf240c9c9428ebe3b20a1da
lorenrodmes <rlorename924@gmail.com>
2023-12-15 17:35:13 -0500

    En este avance se trabajo de manera teórica y práctica la temática de OPERACIONES y Tablas de Verdad

---
+++ Comparaciones.py
@@ -0,0 +1,57 @@
+# > 
+print(7 > 3)
+print(3 > 7)
+print(7 > 7)
+
+# <
+print(5 < 6)
+print(6 < 5)
+print(5 < 5)
+
+# >=
+print(2 >= 1)
+print(2 >= 3)
+print(2 >= 2)
+
+# <=
+print(1 <= 2)
+print(2 <= 1)
+print(2 <= 2)
+
+# ==
+
+print(6 == 6)
+print(5 == 2)
+
+# !=
+
+print(6 != 10)
+print(6 != 6)
+
+print("Apple" == 'Apple')
+print("Apple" == 'apple')
+print("1" == 1)
+
+age = 15
+print(age >= 18)
+
+
+# Comparación de números flotantes
+
+x = 3.3
+print(x)
+y = 1.1 + 2.2
+print(y)
+print(x == y)
+
+y_str = format(y, ".2g")
+print('str =>', y_str)
+print(y_str == str(x))
+
+print('*' * 10)
+
+print(y, x)
+
+tolerance = 0.00001
+print(abs(x - y) < tolerance)
+
